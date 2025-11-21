64a3ba456fd8b6a1e6e10aa09d603a29cb37a5c7
massidip <massimodipietrantonio04@gmail.com>
2025-11-21 12:02:36 +0100

    Modifiche Pom file

---
+++ famigliasapori/src/main/java/famiglia/sapori/FamigliaSaporiApplication.java
@@ -0,0 +1,38 @@
+package famiglia.sapori;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * Applicazione principale per il Gestionale Famiglia Sapori
+ */
+public class FamigliaSaporiApplication extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("main"), 800, 600);
+        stage.setTitle("Gestionale Famiglia Sapori");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(FamigliaSaporiApplication.class.getResource("/fxml/" + fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
